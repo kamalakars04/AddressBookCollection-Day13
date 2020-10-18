1674e222339d8d1d9c5e171c7e43a4151337314f
kamalakars04 <kamalakars04@gmail.com>
2020-10-18 17:51:23 +0530

    [Kamalakar].[Refactor].Added UC7_DuplicateContacts

--- AddressBookDetails.cs
+++ AddressBookDetails.cs
@@ -16,15 +16,16 @@ namespace AddressBookSystem
         const string GET_ALL_CONTACTS = "view";
         public Dictionary<string, AddressBook> addressBookList = new Dictionary<string, AddressBook>();
 
-
+        /// <summary>
+        /// Gets the address book.
+        /// </summary>
+        /// <returns></returns>
         private AddressBook GetAddressBook()
         {
             Console.WriteLine("\nEnter name of Address Book to be accessed or to be added");
             nameOfAddressBook = Console.ReadLine();
 
-            
-
-            //search for address book in dictionary
+            // search for address book in dictionary
             if (addressBookList.ContainsKey(nameOfAddressBook))
             {
                 Console.WriteLine("\nAddressBook Identified");
@@ -32,122 +33,128 @@ namespace AddressBookSystem
                 return addressBookList[nameOfAddressBook];
             } 
 
-            //Offer to create a address book if not found in dictionary
+            // Offer to create a address book if not found in dictionary
+            logger.Warn("AddressBook " + nameOfAddressBook + " not found");
+            Console.WriteLine("\nAddress book not found. Type y to create a new address book or E to abort");
+
+            // If user want to create a new address book
+            if ((Console.ReadLine().ToLower()) == "y")
+            {
+                AddressBook addressBook = new AddressBook(nameOfAddressBook);
+                addressBookList.Add(nameOfAddressBook, addressBook);
+                Console.WriteLine("\nNew AddressBook Created");
+                logger.Info("New address book created with name : " + nameOfAddressBook);
+                return addressBookList[nameOfAddressBook];
+            }
+
+            // If User want to abort the operation 
             else
             {
-                logger.Warn("AddressBook " + nameOfAddressBook + " not found");
-                Console.WriteLine("\nAddress book not found. Type y to create a new address book or E to abort");
-                //If user want to create a new address book
-                if ((Console.ReadLine().ToLower()) == "y")
-                {
-                    AddressBook addressBook = new AddressBook(nameOfAddressBook);
-                    addressBookList.Add(nameOfAddressBook, addressBook);
-                    Console.WriteLine("\nNew AddressBook Created");
-                    logger.Info("New address book created with name : " + nameOfAddressBook);
-                    return addressBookList[nameOfAddressBook];
-                }
-                //If User want to abort the operation 
-                else
-                {
-                    Console.WriteLine("\nAction Aborted");
-                    logger.Info("User aborted the operation to create new Address book with name : " + nameOfAddressBook);
-                    return null;
-                }
-                    
-
+                Console.WriteLine("\nAction Aborted");
+                logger.Info("User aborted the operation to create new Address book with name : " + nameOfAddressBook);
+                return null;
             }
         }
 
+        /// <summary>
+        /// Deletes the address book.
+        /// </summary>
         public void DeleteAddressBook()
         {
             // Returns no record found if address book is empty
             if (addressBookList.Count == 0)
-                Console.WriteLine("No record found");
-            else
             {
-                Console.WriteLine("\nEnter the name of address book to be deleted :");
-
-                //search for address book with given name
-                try
-                {
-                    addressBookList.Remove(Console.ReadLine());
-                    Console.WriteLine("Address book deleted successfully");
-                    logger.Info("User deleted the AddressBook " + nameOfAddressBook);
-                }
-                catch
-                {
-                    Console.WriteLine("Address book not found");
-                }
+                Console.WriteLine("No record found");
+                return;
             }
-            
-               
+            Console.WriteLine("\nEnter the name of address book to be deleted :");
+
+            //search for address book with given name
+            try
+            {
+                addressBookList.Remove(Console.ReadLine());
+                Console.WriteLine("Address book deleted successfully");
+                logger.Info("User deleted the AddressBook " + nameOfAddressBook);
+            }
+            catch
+            {
+                Console.WriteLine("Address book not found");
+            }
         }
 
+        /// <summary>
+        /// Views all address books.
+        /// </summary>
         public void ViewAllAddressBooks()
         {
             // Returns no record found if address book is empty
             if (addressBookList.Count == 0)
-                Console.WriteLine("No record found");
-            else
             {
-                Console.WriteLine("\nThe namesof address books available are :");
-                foreach (KeyValuePair<string, AddressBook> keyValuePair in addressBookList)
-                    Console.WriteLine(keyValuePair.Key);
-                logger.Info("User viewd all AddressBook names");
+                Console.WriteLine("No record found");
+                return;
             }
-            
+
+            // Print the address book names available
+            Console.WriteLine("\nThe namesof address books available are :");
+            foreach (KeyValuePair<string, AddressBook> keyValuePair in addressBookList)
+                Console.WriteLine(keyValuePair.Key);
+            logger.Info("User viewd all AddressBook names");
         }
 
+        /// <summary>
+        /// Adds the or access address book.
+        /// </summary>
         public void AddOrAccessAddressBook()
         {
             // To get the name of the addressbook
             AddressBook addressBook = GetAddressBook();
+
             // Returns no record found if address book is empty
             if (addressBook == null)
             {
                 Console.WriteLine("Action aborted");
                 return;
             }
-        //select the action to be performed in address book   
+
+            // select the action to be performed in address book   
             while(true)
             {
                 Console.WriteLine("\nSelect from below to work on Address book {0}", addressBook.nameOfAddressBook);
-                Console.WriteLine("\nType\n\nAdd - To add a contact \nUpdate- To update a contact\nView - To view all contacts\nRemove - To remove a contact and \nSearch- To search to get contact deatails\nE - To exit\n ");
-
+                Console.WriteLine("\nType\n\nAdd - To add a contact" +
+                                  "\nUpdate- To update a contact" +
+                                  "\nView - To view all contacts" +
+                                  "\nRemove - To remove a contact and " +
+                                  "\nSearch- To search to get contact deatails\nE - To exit\n ");
                 switch (Console.ReadLine().ToLower())
                 {
                     case ADD_CONTACT:
-
                         addressBook.AddContact();
                         break;
 
                     case UPDATE_CONTACT:
-
                         addressBook.UpdateContact();
                         break;
 
                     case SEARCH_CONTACT:
-
                         addressBook.DisplayContactDetails();
                         break;
 
                     case REMOVE_CONTACT:
-
                         addressBook.RemoveContact();
                         break;
 
                     case GET_ALL_CONTACTS:
-
                         addressBook.GetAllContacts();
                         break;
 
                     default:
-
                         Console.WriteLine("\nInvalid option. Exiting address book");
                         return;
                 }
+
                 // Ask the user to continue in same address book or to exit
                 Console.WriteLine("\nType y to continue in same address Book or any other key to exit");
+
                 // If not equal to y  then exit
                 if (!(Console.ReadLine().ToLower() == "y"))
                 {
@@ -155,10 +162,6 @@ namespace AddressBookSystem
                     return;
                 }
             }
-           
-        
         }
-        
-    }
-
+     }
 }
--- ContactDetails.cs
+++ ContactDetails.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace AddressBookSystem
 {
-    class ContactDetails
+    public class ContactDetails
     {
+        // Variables
         public string firstName;
         public string lastName;
         public string address;
@@ -14,7 +15,18 @@ namespace AddressBookSystem
         public string zip;
         public string phoneNumber;
         public string email;
-        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContactDetails"/> class.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        /// <param name="address">The address.</param>
+        /// <param name="city">The city.</param>
+        /// <param name="state">The state.</param>
+        /// <param name="zip">The zip.</param>
+        /// <param name="phoneNumber">The phone number.</param>
+        /// <param name="email">The email.</param>
         public ContactDetails(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
         {
             this.firstName = firstName.ToLower();
@@ -26,6 +38,33 @@ namespace AddressBookSystem
             this.phoneNumber = phoneNumber;
             this.email = email;
         }
-      
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns>
+        ///   <see langword="true" /> if the specified object  is equal to the current object; otherwise, <see langword="false" />.
+        /// </returns>
+        public override bool Equals(Object obj)
+        {
+            // if the list is null
+            if (obj == null)
+                return false;
+
+            // if the object passed is not a list
+            if (!(obj is List<ContactDetails>))
+                return true;
+
+            // Get the contacts from list with same name
+            var duplicates = ((List<ContactDetails>)obj).FindAll(contact => ((contact.firstName).ToLower() == (this.firstName).ToLower()
+                                                                    && (contact.lastName).ToLower() == (this.lastName).ToLower()));
+            
+            // Return true if duplicate is found else false
+            if (duplicates.Count > 0)
+                return true;
+            else
+                return false;
+        }
     }
 }
