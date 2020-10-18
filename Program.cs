9b17a02f8e12995c05fb85c219fec3e442ad479b
kamalakars04 <kamalakars04@gmail.com>
2020-10-19 00:13:42 +0530

    [Kamalakar].[Refactor].Added UC10_CountByCityOrState

--- Program.cs
+++ Program.cs
@@ -15,14 +15,16 @@
 
     public class Program
     {
-        public const string TO_ADD_OR_ACCESS = "add";
+        public const string TO_ADD_OR_ACCESS = "a";
         public const string TO_VIEW_ALL_ADDRESSBOOKS = "view";
         public const string TO_DELETE_ADDRESS_BOOK = "delete";
         public const string SEARCH_PERSON_IN_CITY = "city";
         public const string SEARCH_PERSON_IN_STATE = "state";
         public const string VIEW_ALL_IN_CITY = "vcity";
         public const string VIEW_ALL_IN_STATE = "vstate";
-
+        public const string COUNT_ALL_IN_CITY = "ccity";
+        public const string COUNT_ALL_IN_STATE = "cstate";
+        public const string EXIT = "e";
 
 
         /// <summary>
@@ -37,14 +39,16 @@
             while (flag)
             {
                 Console.WriteLine("\nType to select address book" +
-                                  "\nAdd - To add or access address book" +
-                                  "\nview - To view all names of address books" +
+                                  "\nA      - To add or access address book" +
+                                  "\nview   - To view all names of address books" +
                                   "\nDelete - To delete Address book" +
-                                  "\nCity - To search contact in a city" +
-                                  "\nState - To search contact in a state" +
-                                  "\nVCity - To view all contacts in a city" +
+                                  "\nCity   - To search contact in a city" +
+                                  "\nState  - To search contact in a state" +
+                                  "\nVCity  - To view all contacts in a city" +
                                   "\nVState - To view all contacts in a state" +
-                                  "\nE - To exit");
+                                  "\nCCity  - To get count of contacts city wise" +
+                                  "\nCState - To get count of contacts state wise" +
+                                  "\nE      - To exit\n\n");
                 switch (Console.ReadLine().ToLower())
                 {
                     // To add or access new Address book
@@ -81,10 +85,23 @@
                     case VIEW_ALL_IN_STATE:
                         addressBookDetails.ViewAllByState();
                         break;
-                    default:
+
+                    // To get count of contacts in a city
+                    case COUNT_ALL_IN_CITY:
+                        addressBookDetails.CountAllByCity();
+                        break;
+
+                    // To get count of contacts in a state
+                    case COUNT_ALL_IN_STATE:
+                        addressBookDetails.CountAllByState();
+                        break;
+                    case EXIT:
                         Console.WriteLine("User exited application");
                         flag = false;
                         return;
+                    default:
+                        Console.WriteLine("Invalid entry");
+                        break;
                 }
             }
         }
